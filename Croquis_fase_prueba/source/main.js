07483fe3f5416206aa0aa4229d0ad2fc408ac708
Alejandro Torres <aleth2k@gmail.com>
2024-03-03 22:40:27 -0600

    se creo una version de prueba para el croquis 3d usando three.js

---
+++ Croquis_fase_prueba/source/main.js
@@ -0,0 +1,39 @@
+import './style.css';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+const scene = new THREE.Scene(); /*escena*/
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);/*fov, aspect, near, far*/ /*Fov = Campo de visión, aspect = aspecto, near: cercania, far: lejania*/
+const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector('#bg'), /* Selector de canvas para rederizado de GPU*/ 
+});
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+const controls = new OrbitControls( camera, renderer.domElement ); /*Controles para usar el maouse*/
+
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100); /*geometria*/
+const material = new THREE.MeshBasicMaterial({color: 0xFF6347/*, wireframe: true*/}); /*material*/ 
+const torus = new THREE.Mesh(geometry, material); /*malla*/
+
+const pointLight = new THREE.PointLight(0xffffff); /*Punto de luz*/
+pointLight.position.set(5,5,5);
+const ambientLight = new THREE.AmbientLight(0xffffff); /*Luz ambiental*/
+const lightHelper = new THREE.PointLightHelper(pointLight); /*Debug purpose*/
+const gridHelper = new THREE.GridHelper(200,50); /*Debug purpose*/
+scene.add(pointLight, ambientLight, lightHelper, gridHelper);
+
+scene.add(torus);
+
+function gameLoop() { /*ciclo infinito para mostrar la escena y el renderizado*/
+    requestAnimationFrame(gameLoop);
+    controls.update(); /*actualizar la pocicion de los controles para que sean funcionales*/
+    renderer.render(scene, camera);
+}
+
+gameLoop();
+
+// const loader = new GLTFLoader();
+
